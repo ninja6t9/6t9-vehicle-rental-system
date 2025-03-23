c3f5196d910acdd697ad039472ff9942d6617dc6
kalana ranathunga <kalanaranathunga5@gmail.com>
2025-03-23 10:13:17 +0530

    ....

---
+++ config.lua
@@ -0,0 +1,14 @@
+Config = {}
+
+Config.RentalLocations = {
+    { x = -56.92, y = -1096.76, z = 26.42 }, -- Example rental location
+}
+
+Config.RentalVehicles = {
+    { model = 'futo', price = 500 },
+    { model = 'blista', price = 300 },
+    { model = 'sultan', price = 1000 }
+}
+
+Config.FineAmount = 1000 -- Fine if the vehicle is not returned
+Config.InsurancePrice = 200 -- Extra cost for damage protection
